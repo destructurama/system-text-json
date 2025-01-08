d52e4963f5bd240b5d9b299ed26985e407f1ddb9
github-action-benchmark <github@users.noreply.github.com>
2025-01-08 20:41:38 +0000

    add Benchmarks (benchmarkdotnet) benchmark result for 1fdfb44ddbc72a337bbf91c6f25183e019e2f2a8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735923087243,
+  "lastUpdate": 1736368898086,
   "repoUrl": "https://github.com/destructurama/system-text-json",
   "entries": {
     "Benchmarks": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 2370.7125361515928,
             "unit": "ns",
             "range": "± 23.16579889416008"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1fdfb44ddbc72a337bbf91c6f25183e019e2f2a8",
+          "message": "Bump xunit from 2.9.2 to 2.9.3 in /src (#2)\n\nBumps [xunit](https://github.com/xunit/xunit) from 2.9.2 to 2.9.3.\r\n- [Commits](https://github.com/xunit/xunit/compare/v2-2.9.2...v2-2.9.3)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: xunit\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-01-08T23:40:34+03:00",
+          "tree_id": "0b911cedd2f1feca5604e0fbf2cbd740c94fba04",
+          "url": "https://github.com/destructurama/system-text-json/commit/1fdfb44ddbc72a337bbf91c6f25183e019e2f2a8"
+        },
+        "date": 1736368897215,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.SystemTextJsonBenchmarks.Destructure",
+            "value": 2465.207378114973,
+            "unit": "ns",
+            "range": "± 20.152763333939124"
           }
         ]
       }
